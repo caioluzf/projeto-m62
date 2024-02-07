c5b4be2df26f14a59e09bbbae58fc19f00ee9fdb
caioluzf <159257006+caioluzf@users.noreply.github.com>
2024-02-07 09:30:35 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let usuario = document.getElementById("usuario").Value;
+let senha = document.getElementById("senha").Value;
+
+function validacampos(){
+    alert("usuario");
+}
+if(usuario!= "juca" && senha != "123456"){
+    alert("usuario ou senha invalidos")
+    else{
+        alert("seja bem vindo",ususario)
+    }
+}
+
